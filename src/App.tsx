41a9d3dd1189e3bdfdab9342c40dab462c9d4f2b
stellinha16 <stellagiovana846@gmail.com>
2023-09-22 10:46:51 -0400

    certo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './componentes/header/Header'
+import Main from './componentes/main/Main'
+import Footer from './componentes/footer/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+      <Main/>
+    </div>
+  );
+  }
+
+export default App
